827be2057c1f98bed5d01379ceab4ab11a4b6097
chlucas324 <chlucas324@gmail.com>
2022-07-15 10:57:42 -0400

    update components

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
+
   return {
     headers: {
       ...headers,
@@ -66,7 +67,7 @@ function App() {
                 element={<SingleThought />} 
               />
               <Route 
-                path="*" 
+                path="*"
                 element={<NoMatch />} 
               />
             </Routes>
